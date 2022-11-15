97e3aef03b52d7ed16c366615e5c287c7b71f511
MFaizanTariq <114020790+MFaizanTariq@users.noreply.github.com>
2022-11-14 19:49:26 -0500

    Update auths.py

--- auths.py
+++ auths.py
@@ -51,8 +51,7 @@ def callback():
 
     credentials = flow.credentials
     request_session = requests.session()
-    cached_session = cachecontrol.CacheControl(request_session)
-    token_request = google.auth.transport.requests.Request(session=cached_session)
+    token_request = google.auth.transport.requests.Request(session=request_session)
 
     id_info = id_token.verify_oauth2_token(
         id_token=credentials._id_token,
