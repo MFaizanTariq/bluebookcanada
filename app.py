2280f34dee4649677c03bbcd306f524f2687cdd7
MFaizanTariq <114020790+MFaizanTariq@users.noreply.github.com>
2022-11-14 23:22:54 -0500

    Update app.py
    facebook authorization libraries addition

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from auths import auths
 from models import news_fetch1, news_fetch2
 from keys import key1, key2, key3, key4, key5, key6, key7, key8, key9, key10, key11, key12, key13, key14, key15, key16, key17, key18, key19, key20, key21, key22, key23
 from flask_apscheduler import APScheduler
+from authlib.integrations.flask_client import OAuth
 import datetime
 import sqlite3
 
@@ -17,6 +18,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ProjectBlueBook'
 app.config.from_object(Config())
 app.config['SQLACHEMY_DATABASE_URI'] = 'postgres://adffdgsrywxhsa:d483d23a6d3ac240757343c3b1ab826b05e8211a219306a9b271da8b964ee6f7@ec2-54-174-31-7.compute-1.amazonaws.com:5432/ddi35hde1c4juj'
+oauth = OAuth(app)
 
 Bootstrap(app)
 scheduler = APScheduler()
