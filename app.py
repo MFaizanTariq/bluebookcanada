fd240727b33ada00e79c726afc026c2d21097a46
MFaizanTariq <114020790+MFaizanTariq@users.noreply.github.com>
2022-11-15 21:57:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ app.register_blueprint(views, url_prefix='/')
 app.register_blueprint(auths, url_prefix='/')
 
 
-@scheduler.task('cron', id='1', hour='22', minute='02')
+@scheduler.task('cron', id='1', hour='06', minute='02')
 def news_fetch():
     news_fetch1('entertainment', 'australia', key11)
     news_fetch1('business', 'australia', key11)
